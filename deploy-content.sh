67ba3f9dde55f6c7c9aa1f9b133d7e40dc0d4256
MARKS, ZACHARY <zacharymarks@northwesternmutual.com>
2021-04-13 16:37:42 -0500

    Log url

--- deploy-content.sh
+++ deploy-content.sh
@@ -8,10 +8,12 @@ region='us-east-1'
 export AWS_DEFAULT_REGION=$region
 export AWS_REGION=$region
 
-s3_bucket_key='first-website-content-bucket'
+project_key='first-website'
 
 set -x
 
-bucket=$(aws cloudformation list-exports --query "Exports[?Name=='${s3_bucket_key}'].Value" --output text)
+bucket=$(aws cloudformation list-exports --query "Exports[?Name=='${project_key}-content-bucket'].Value" --output text)
+URL=$(aws cloudformation list-exports --query "Exports[?Name=='${project_key}-domain'].Value" --output text)
 
 aws s3 sync src s3://${bucket}
+echo "URL: $URL"
